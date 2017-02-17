71801550ecc5a4d15e5515e9bf5f12d22fce850a
Mohammad Merajul Islam Molla <merajul.molla@sekai-lab-bd.com>
2017-02-17 10:29:20 +0600

    Add KV.Bucket module and functions Add test for KV.Bucket module
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- kv/lib/kv.ex
+++ kv/lib/kv.ex
@@ -2,17 +2,4 @@ defmodule KV do
   @moduledoc """
   Documentation for KV.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> KV.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
