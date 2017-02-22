0be5dd08abe09d75b94bf023169df0abe254cac1
Mohammad Merajul Islam Molla <merajul.molla@sekai-lab-bd.com>
2017-02-22 15:27:11 +0600

    Basic implementation of temperate retrieval for a location
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- metex/lib/metex.ex
+++ metex/lib/metex.ex
@@ -1,18 +1,38 @@
-defmodule Metex do
-  @moduledoc """
-  Documentation for Metex.
-  """
+defmodule Metex.Worker do
 
-  @doc """
-  Hello world.
+    def temperature_of(location) do
+        result = url_for(location) |> HTTPoison.get |> parse_response
+        case result do
+            {:ok, temp} ->
+                "#{location}: #{temp}°C"
+            :error ->
+                "#{location} not found"
+        end
+    end
 
-  ## Examples
+    defp url_for(location) do
+        location = URI.encode(location)
+        "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey}"
+    end
 
-      iex> Metex.hello
-      :world
+    defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+        body |> JSON.decode! |> compute_temperature
+    end
 
-  """
-  def hello do
-    :world
-  end
+    defp parse_response(_) do
+         :error
+    end
+
+    defp compute_temperature(json) do
+         try do
+            temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+            {:ok, temp}
+        rescue
+            _ -> :error
+        end
+    end
+
+    defp apikey do
+        "acb2904895792f9b86627f293c081bb5"
+    end
 end
