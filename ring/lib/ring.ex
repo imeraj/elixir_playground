8ab56b92a0aca63238de7f9f4c8a43c63c2d199e
Mohammad Merajul Islam Molla <meraj.enigma@gmail.com>
2017-02-27 22:14:48 +0600

    implement process linking
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- ring/lib/ring.ex
+++ ring/lib/ring.ex
@@ -1,18 +1,33 @@
 defmodule Ring do
-  @moduledoc """
-  Documentation for Ring.
-  """
+    def create_processes(n) do
+        1..n |> Enum.map(fn _ -> spawn(fn ->
+          loop end)
+        end)
+    end
 
-  @doc """
-  Hello world.
+    def loop do
+      receive do
+        {:link, link_to} when is_pid(link_to) ->
+          Process.link(link_to)
+          loop()
+        :crash ->
+          1/0
+      end
+    end
 
-  ## Examples
+    def link_processes(procs) do
+      link_processes(procs, [])
+    end
 
-      iex> Ring.hello
-      :world
+    def link_processes([proc_1, proc_2|rest], linked_processes) do
+      send(proc_1, {:link, proc_2})
+      link_processes([proc_2|rest], [proc_1|linked_processes])
+    end
 
-  """
-  def hello do
-    :world
-  end
+    def link_processes([proc|[]], linked_processes) do
+      first_process = linked_processes |> List.last
+  
+      send(proc, {:link, first_process})
+      :ok
+    end
 end
