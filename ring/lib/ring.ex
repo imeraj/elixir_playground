11d157f4874078a27ad833adaa7085accfc2268b
Mohammad Merajul Islam Molla <meraj.enigma@gmail.com>
2017-02-27 20:19:46 +0600

    Initial commit "ring"
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

---
+++ ring/lib/ring.ex
@@ -0,0 +1,18 @@
+defmodule Ring do
+  @moduledoc """
+  Documentation for Ring.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ring.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
