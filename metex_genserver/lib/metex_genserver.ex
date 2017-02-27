af40ed31968ea396937aeb3a89ae71b0b530d524
Mohammad Merajul Islam Molla <merajul.molla@sekai-lab-bd.com>
2017-02-27 13:26:56 +0600

    Initial commit
    Signed-off-by: Meraj <meraj.enigma@gmailc.com>

---
+++ metex_genserver/lib/metex_genserver.ex
@@ -0,0 +1,18 @@
+defmodule MetexGenserver do
+  @moduledoc """
+  Documentation for MetexGenserver.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MetexGenserver.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
