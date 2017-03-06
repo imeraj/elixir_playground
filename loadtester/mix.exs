a61d96c8f858e85f803621ecf03bb397c02670d4
Mohammad Merajul Islam Molla <meraj.enigma@gmail.com>
2017-03-06 20:26:45 +0600

    Fix mix.exs file
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- loadtester/mix.exs
+++ loadtester/mix.exs
@@ -16,7 +16,8 @@ defmodule Loadtester.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :timex, :httpoison]]
+    [extra_applications: [:logger, :timex, :httpoison],
+    mod: {Loadtester, []}]
   end
 
   # Dependencies can be Hex packages:
