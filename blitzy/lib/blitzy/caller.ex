3a65932a942b2eb1134efcae900a75fa07ff4b1a
iMeraj-MacbookPro <meraj@Enigma-Meraj.local>
2017-06-10 23:21:03 +0600

    blitzy: parse_results function added
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- blitzy/lib/blitzy/caller.ex
+++ blitzy/lib/blitzy/caller.ex
@@ -6,6 +6,45 @@ defmodule Blitzy.Caller do
        1..n_workers
        |> Enum.map(fn _ -> Task.async(worker_func) end)
        |> Enum.map(&Task.await(&1, :infinity))
+       |> parse_results
    end
 
+   defp parse_results(results) do
+       {successes, failures} =
+         results
+         |> Enum.partition(fn x ->
+            case x do
+                {:ok, _} -> true
+                _ -> false
+            end
+         end)
+
+       total_workers = Enum.count(results)
+       total_success = Enum.count(successes)
+       total_failure = total_workers - total_success
+
+       data = successes |> Enum.map(fn {:ok, time} -> time end)
+       average_time = average(data)
+       longest_time = Enum.max(data)
+       shortest_time = Enum.min(data)
+
+       IO.puts """
+       Total workers        : #{total_workers}
+       Successful reqs      : #{total_success}
+       Failed res           : #{total_failure}
+       Average (msecs)      : #{average_time}
+       Longest (msecs)      : #{longest_time}
+       Shortest (msecs)     : #{shortest_time}
+       """
+   end
+
+   defp average(list) do
+     sum = Enum.sum(list)
+
+     if sum > 0 do
+        sum / Enum.count(list)
+     else
+        0
+     end
+   end
 end
