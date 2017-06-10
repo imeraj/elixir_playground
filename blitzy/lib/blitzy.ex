3a76aba3a5d15a78ae8d4515e1784a07394e691e
iMeraj-MacbookPro <meraj@Enigma-Meraj.local>
2017-06-10 19:34:02 +0600

    blitzy: initial commit

---
+++ blitzy/lib/blitzy.ex
@@ -0,0 +1,18 @@
+defmodule Blitzy do
+  @moduledoc """
+  Documentation for Blitzy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blitzy.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
