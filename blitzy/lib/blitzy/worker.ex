193517ffff5c25156a81fd50488697e68d470ab7
iMeraj-MacbookPro <meraj@Enigma-Meraj.local>
2017-06-11 19:10:49 +0600

    blitzy: indentation fix
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- blitzy/lib/blitzy/worker.ex
+++ blitzy/lib/blitzy/worker.ex
@@ -2,17 +2,17 @@ defmodule Blitzy.Worker do
   use Timex
   require Logger
 
-  def start(url) do
-    IO.puts "Running on #node - #{node()}"
-    {timestamp, response} = Duration.measure(fn -> HTTPoison.get(url) end)
-    handle_response({Duration.to_milliseconds(timestamp), response})
-  end
+   def start(url) do
+     IO.puts "Running on #node - #{node()}"
+     {timestamp, response} = Duration.measure(fn -> HTTPoison.get(url) end)
+     handle_response({Duration.to_milliseconds(timestamp), response})
+   end
 
-  defp handle_response({msecs, {:ok, %HTTPoison.Response{status_code: code}}})
-  when code >= 200 and code <= 304 do
+   defp handle_response({msecs, {:ok, %HTTPoison.Response{status_code: code}}})
+   when code >= 200 and code <= 304 do
       Logger.info "worker [#{node()}-#{inspect self()}] completed in #{msecs} msecs"
       {:ok, msecs}
-  end
+   end
 
    defp handle_response({_msecs, {:error, %HTTPoison.Error{reason: reason}}}) do
      Logger.info "worker [#{node()}-#{inspect self()}] error due to #{inspect reason}"
