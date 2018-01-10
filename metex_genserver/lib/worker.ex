ca8a78eeda873efdaaf075637ed13e38071c6c91
Meraj <meraj.enigma@gmail.com>
2018-01-10 23:12:18 +0600

    Mostly refactored. Implemented get_stats and reset_stats properly
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- metex_genserver/lib/worker.ex
+++ metex_genserver/lib/worker.ex
@@ -3,9 +3,13 @@ defmodule MetexGenserver.Worker do
 
     @name MW
 
+    defp apikey do
+	    "acb2904895792f9b86627f293c081bb5"
+    end
+
     ## Client API
     def start_link(options \\ []) do
-        GenServer.start_link(__MODULE__, :ok, options ++ [name: MW])
+        GenServer.start_link(__MODULE__, :ok, options ++ [name: @name])
     end
 
     def get_temperature(location) do
@@ -13,7 +17,11 @@ defmodule MetexGenserver.Worker do
     end
 
     def reset_stats do
-        GenServer.cast(@name, :reset_stats)
+        GenServer.call(@name, :reset_stats)
+    end
+
+    def get_stats do
+        GenServer.call(@name, :get_stats)
     end
 
     def stop do
@@ -21,41 +29,15 @@ defmodule MetexGenserver.Worker do
     end
 
     def terminate(reason, _stats) do
-        IO.puts "server terminated because of #{inspect reason}"
-        :ok
-    end
-
-    ## Server Callbacks
-    def init(:ok) do
-        {:ok, %{}}
-    end
-
-    def handle_call({:location, location}, _from, stats) do
-        case temperature_of(location) do
-            {:ok, temp} ->
-                new_stats = update_stats(stats, location)
-                {:reply, "#{temp}°C", new_stats}
-            _ ->
-                {:reply, :error, stats}
-        end
-    end
-
-    def handle_cast(:reset_stats, _stats) do
-        {:noreply, %{}}
-    end
-
-    def handle_cast(:stop, stats) do
-        {:stop, :normal, stats}
-    end
-
-    def handle_info(msg, stats) do
-        IO.puts "received #{inspect msg}"
-        {:noreply, stats}
+	    IO.puts "server terminated because of #{inspect reason} reason"
+	    :ok
     end
 
     ## Helper Functions
     defp temperature_of(location) do
-        url_for(location) |> HTTPoison.get |> parse_response
+        url_for(location)
+        |> HTTPoison.get
+        |> parse_response
     end
 
     defp url_for(location) do
@@ -73,14 +55,15 @@ defmodule MetexGenserver.Worker do
 
     defp compute_temperature(json) do
         try do
-            temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+            temp = (json["main"]["temp"] - 273.15)
+                   |> Float.round(1)
             {:ok, temp}
         rescue
             _ -> :error
         end
     end
 
-    def update_stats(old_stats, location) do
+    defp update_stats(old_stats, location) do
         case Map.has_key?(old_stats, location) do
             true ->
                 Map.update!(old_stats, location, &(&1 + 1))
@@ -89,7 +72,35 @@ defmodule MetexGenserver.Worker do
         end
     end
 
-    def apikey do
-        "acb2904895792f9b86627f293c081bb5"
+    ## Server Callbacks
+    def init(:ok) do
+	    {:ok, %{}}
+    end
+
+    def handle_call({:location, location}, _from, stats) do
+	    case temperature_of(location) do
+		    {:ok, temp} ->
+			    new_stats = update_stats(stats, location)
+			    {:reply, "#{temp}°C", new_stats}
+		    _ ->
+			    {:reply, :error, stats}
+	    end
+    end
+
+    def handle_call(:get_stats, _from, stats) do
+	    {:reply, stats, stats}
+    end
+
+    def handle_call(:reset_stats, _from, _stats) do
+	    {:reply, %{}, %{}}
+    end
+
+    def handle_cast(:stop, stats) do
+	    {:stop, :normal, stats}
+    end
+
+    def handle_info(msg, stats) do
+	    IO.puts "received #{inspect msg}"
+	    {:noreply, stats}
     end
 end
