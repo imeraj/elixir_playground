14dd59f45bd983eedf0285db56d36e74f65844a5
Meraj <meraj.enigma@gmail.com>
2021-10-03 15:01:31 -0400

    - setup_all added for response_parser_test

--- testing_elixir/unit_tests/soggy_waffle/test/soggy_waffle/weather_api/response_parser_test.exs
+++ testing_elixir/unit_tests/soggy_waffle/test/soggy_waffle/weather_api/response_parser_test.exs
@@ -3,22 +3,22 @@ defmodule SoggyWaffle.WeatherAPI.ResponseParserTest do
   alias SoggyWaffle.WeatherAPI.ResponseParser
   alias SoggyWaffle.Weather
 
-  describe "parse_response/1" do
-    test "success: accepts a valid response, returns a list of structs" do
-      api_response = %{
-        "list" => [
-          %{"dt" => 1_574_359_200, "weather" => [%{"id" => 600}]},
-          %{"dt" => 1_574_359_900, "weather" => [%{"id" => 299}]}
-        ]
-      }
+  setup_all do
+    response_as_map =
+      File.read!("test/support/weather_api_response.json")
+      |> Jason.decode!()
 
-      assert {:ok, parsed_response} = ResponseParser.parse_response(api_response)
+    %{weather_data: response_as_map}
+  end
+
+  describe "parse_response/1" do
+    test "success: accepts a valid response, returns a list of structs", %{weather_data: weather_data} do
+      assert {:ok, parsed_response} = ResponseParser.parse_response(weather_data)
 
       for weather_record <- parsed_response do
         assert match?(%Weather{datetime: %DateTime{}, rain?: _rain}, weather_record)
         assert is_boolean(weather_record.rain?)
       end
-
     end
   end
 end
