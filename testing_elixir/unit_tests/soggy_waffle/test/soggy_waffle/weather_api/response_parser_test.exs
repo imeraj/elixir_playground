ca1005675d33eaf3658c12d75457db48db5ceef2
Meraj <meraj.enigma@gmail.com>
2021-10-03 15:10:04 -0400

    - leverage list comprehension

--- testing_elixir/unit_tests/soggy_waffle/test/soggy_waffle/weather_api/response_parser_test.exs
+++ testing_elixir/unit_tests/soggy_waffle/test/soggy_waffle/weather_api/response_parser_test.exs
@@ -3,6 +3,13 @@ defmodule SoggyWaffle.WeatherAPI.ResponseParserTest do
   alias SoggyWaffle.WeatherAPI.ResponseParser
   alias SoggyWaffle.Weather
 
+  @thunderstorm_ids {
+    "thunderstorm",
+    [200, 201, 202, 210, 211, 212, 221, 230, 231, 232]
+  }
+  @drizzle_ids {"drizzle", [300, 301, 302, 310, 311, 312, 313, 314, 321]}
+  @rain_ids {"rain", [500, 501, 502, 503, 504, 511, 520, 521, 522, 531]}
+
   setup_all do
     response_as_map =
       File.read!("test/support/weather_api_response.json")
@@ -21,4 +28,18 @@ defmodule SoggyWaffle.WeatherAPI.ResponseParserTest do
       end
     end
   end
+
+  for {condition, ids} <- [@thunderstorm_ids, @drizzle_ids, @rain_ids] do
+    test "success: recognizes #{condition} as a rainy condition" do
+      now_unix = DateTime.utc_now() |> DateTime.to_unix()
+
+      for id <- unquote(ids) do
+        record = %{"dt" => now_unix, "weather" => [%{"id" => id}]}
+
+        assert {:ok, [weather_struct]} = ResponseParser.parse_response(%{"list" => [record]})
+        assert weather_struct.rain? == true, "Expected weather id (#{id}) to be a rain condition"
+      end
+    end
+  end
+
 end
