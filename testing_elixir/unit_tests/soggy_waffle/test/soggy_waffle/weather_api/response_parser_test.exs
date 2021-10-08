85db5e996185a63c81b3712d14b1f02a04e148b2
Meraj <meraj.enigma@gmail.com>
2021-10-08 08:52:42 -0400

    - add error tests

--- testing_elixir/unit_tests/soggy_waffle/test/soggy_waffle/weather_api/response_parser_test.exs
+++ testing_elixir/unit_tests/soggy_waffle/test/soggy_waffle/weather_api/response_parser_test.exs
@@ -19,11 +19,18 @@ defmodule SoggyWaffle.WeatherAPI.ResponseParserTest do
   end
 
   describe "parse_response/1" do
-    test "success: accepts a valid response, returns a list of structs", %{weather_data: weather_data} do
-      assert {:ok, parsed_response} = ResponseParser.parse_response(weather_data)
+    test "success: accepts a valid response, returns a list of structs", %{
+      weather_data: weather_data
+    } do
+      assert {:ok, parsed_response} =
+               ResponseParser.parse_response(weather_data)
 
       for weather_record <- parsed_response do
-        assert match?(%Weather{datetime: %DateTime{}, rain?: _rain}, weather_record)
+        assert match?(
+                 %Weather{datetime: %DateTime{}, rain?: _rain},
+                 weather_record
+               )
+
         assert is_boolean(weather_record.rain?)
       end
     end
@@ -36,8 +43,11 @@ defmodule SoggyWaffle.WeatherAPI.ResponseParserTest do
       for id <- unquote(ids) do
         record = %{"dt" => now_unix, "weather" => [%{"id" => id}]}
 
-        assert {:ok, [weather_struct]} = ResponseParser.parse_response(%{"list" => [record]})
-        assert weather_struct.rain? == true, "Expected weather id (#{id}) to be a rain condition"
+        assert {:ok, [weather_struct]} =
+                 ResponseParser.parse_response(%{"list" => [record]})
+
+        assert weather_struct.rain? == true,
+               "Expected weather id (#{id}) to be a rain condition"
       end
     end
   end
@@ -53,8 +63,43 @@ defmodule SoggyWaffle.WeatherAPI.ResponseParserTest do
     for id <- 100..900, id not in all_rain_ids do
       record = %{"dt" => now_unix, "weather" => [%{"id" => id}]}
 
-      assert {:ok, [weather_struct]} = ResponseParser.parse_response(%{"list" => [record]})
-      assert weather_struct.rain? == false, "Expected weather id (#{id}) to NOT be a rain condition"
+      assert {:ok, [weather_struct]} =
+               ResponseParser.parse_response(%{"list" => [record]})
+
+      assert weather_struct.rain? == false,
+             "Expected weather id (#{id}) to NOT be a rain condition"
     end
+  end
+
+  test "error: returns error if weather data is malformed" do
+    malformed_day = %{
+      "dt" => DateTime.utc_now() |> DateTime.to_unix(),
+      "weather" => [
+        %{
+          "wrong_key" => 1
+        }
+      ]
+    }
+
+    almost_correct_response = %{"list" => [malformed_day]}
+
+    assert {:error, :response_format_invalid} =
+             ResponseParser.parse_response(almost_correct_response)
+  end
+
+  test "error: returns error if timestamp is missing" do
+    malformed_day = %{
+      "datetime" => DateTime.utc_now() |> DateTime.to_unix(),
+      "weather" => [
+        %{
+          "main" => 1
+        }
+      ]
+    }
+
+    almost_correct_response = %{"list" => [malformed_day]}
+
+    assert {:error, :response_format_invalid} =
+             ResponseParser.parse_response(almost_correct_response)
   end
 end
